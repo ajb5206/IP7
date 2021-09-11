3b42dd082658f4b9beaf4688076fb64caf3cd199
Alex Bertotto <ajb5206@gmail.com>
2021-09-11 12:59:57 -0700

    add after passing initial tests for both Bread and Pastry classes and methods

---
+++ Bakery.Solution/Bakery.Tests/Model.Tests/BakeryTests.cs
@@ -0,0 +1,27 @@
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Bakery;
+
+namespace Bakery.Tests 
+{
+	[TestClass]
+	public class BreadTests
+	{
+		[TestMethod]
+		public void BreadCost_ReturnsCostTimesAmount_Five()
+		{
+			Bread testBread = new Bread();
+			Assert.AreEqual(5, testBread.BreadCost(1));
+		}
+	}
+	[TestClass]
+	public class PastryTests
+	{
+		[TestMethod]
+		public void PastryCost_ReturnsCostTimesAmount_Two()
+		{
+			Pastry testPastry = new Pastry();
+			Assert.AreEqual(2, testPastry.PastryCost(1));
+		}
+	}
+}
---
+++ Bakery.Solution/Bakery/Models/Bakery.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace Bakery
+{
+	public class Bread
+	{
+		public int BreadCost(int bAmount)
+		{
+			return 5;
+		}
+	}
+
+	public class Pastry
+	{
+		public int PastryCost(int pAmount)
+		{
+			return 2;
+		}
+	}
+}
