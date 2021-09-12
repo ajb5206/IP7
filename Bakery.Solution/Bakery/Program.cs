0b958e3d0029a6b1d1af42454a82da7994d96c25
Alex Bertotto <ajb5206@gmail.com>
2021-09-12 01:11:37 -0700

    reworded instatiation of objects in Program

--- Bakery.Solution/Bakery/Program.cs
+++ Bakery.Solution/Bakery/Program.cs
@@ -7,14 +7,15 @@ namespace Bakery
 	{
 		public static void Main()
 		{
-			Bread userBread = new Bread();
-			Pastry userPastry = new Pastry();
 			Console.WriteLine("Welcome to the bakery!");
 			Console.WriteLine("How many loaves of bread would you like to buy?");
 			string loaves = Console.ReadLine();
 			int bAmount = int.Parse(loaves);
+			Bread userBread = new Bread(bAmount);
 			Console.WriteLine("How many pastries would you like to buy?");
 			string pastries = Console.ReadLine();
+			int pAmount = int.Parse(pastries);
+			Pastry userPastry = new Pastry(pAmount);
 			int total = userBread.GetBreadTotal() + userPastry.GetPastryTotal();
 			Console.WriteLine($"Your total is ${total}, thank you!");
 			
